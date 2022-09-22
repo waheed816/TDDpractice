f03c3d6e3442a4ec9ea508d08fa685537a28db1d
Mehedi Meem <waheed816@gmail.com>
2022-09-22 19:30:41 -0400

    myMap solved

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,13 @@
 function myMap(inputArray, callback) {
   // Your code here
+  let newArray = [];
+  let current;
+
+  for(let i = 0; i < inputArray.length; i++){
+    current = inputArray[i];
+    newArray.push(callback(current));
+  }
+  return newArray;
 }
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,27 @@
 // Your code here
+const chai = require("chai");
+const expect = chai.expect;
+
+const myMap = require('../problems/my-map');
+
+describe("call the passed in function on each element of the array", function(){
+    let array;
+    let newArray;
+    let result;
+
+    beforeEach(() => {
+        array = [1, 2, 3];
+        const callback = (el) => el * 2;
+        result = myMap(array, callback);
+    })
+
+    it('should not mutate the array', function() {
+        expect(array).to.deep.equal([1, 2, 3]);
+
+    })
+
+    it('the callback should be applied in myMap', function() {
+        expect(result).to.deep.equal([2, 4, 6]);
+    })
+
+})
