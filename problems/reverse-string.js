6ef43cfad6ef2e943b140c1cf5db7dc6cdf5f2aa
Mehedi Meem <waheed816@gmail.com>
2022-09-22 17:26:23 -0400

    phase 1 done

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,8 @@
-module.exports = function reverseString(string) {
+
+
+function reverseString(string) {
   // Your code here
+  return string.split('').reverse().join('');
 };
+
+module.exports = reverseString;
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,35 @@
-// Your code here
+// // Your code here
+// const chai = require('chai');
+// const expect = chai.expect;
+// const reverseString = require(`../problems/reverse-string`);
+
+//     describe("reverseString", function() {
+//         it("should return the string reversed", function() {
+//             let expected = "nuf";
+//             let reverse = reverseString("fun");
+//             expect(reverse).to.equal(expected);
+
+//         })
+
+//         it("should be a string"), function() {
+//             let number = 2;
+//             expect (() => reverseString(number)).to.throw(Error);
+//         }
+//     })
+
+const chai = require("chai");
+const expect = chai.expect;
+
+const reverseString = require('../problems/reverse-string')
+
+describe('reverse string', function () {
+    it('should reverse given word', function () {
+        let expectedString = 'nuf'
+        let reversed = reverseString('fun')
+        expect(reversed).to.equal(expectedString)
+    })
+    it('should be a string', function () {
+        let number = 4
+        expect(() => reverseString(number)).to.throw(Error)
+    })
+})
